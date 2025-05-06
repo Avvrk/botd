783c8910987560c486e2ebee234c3c87f4ef4f31
Avvrk <jomaldonado480@gmail.com>
2025-05-06 16:17:20 -0500

    e

--- index.mjs
+++ index.mjs
@@ -2,18 +2,19 @@ import "./bot.mjs";
 import express from "express";
 import cors from "cors";
 import db_connection from "./database/connection.mjs";
+import Bot from "./routes/bot.mjs"
 import "dotenv/config";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/bot", Bot);
 
 app.get("/", (req, res) => {
   res.send("Bot de Discord activo.");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, async () => {
+app.listen(process.env.PORT, async () => {
   try {
 		await db_connection();
 		console.log(`Puerto: ${process.env.PORT}`);
