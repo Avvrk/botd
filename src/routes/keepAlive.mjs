370dbfc83ab726d1ad73dfe626f3a83f2b58238d
Jose M. Vargas <137236209+Avvrk@users.noreply.github.com>
2025-05-11 08:49:53 -0500

    pequeño ajuste

--- src/routes/keepAlive.mjs
+++ src/routes/keepAlive.mjs
@@ -3,6 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/ping", (req, res) => {
+  res.end();
   setTimeout(async () => {
     await axios.get("https://experimento-5nzk.onrender.com/ping");
   }, 5 * 60 * 1000);
